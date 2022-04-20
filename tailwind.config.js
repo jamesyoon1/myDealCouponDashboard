fe4538e2a54f4ac778b1c46e2792b3ca6e77d2fa
James Yoon <65934713+jamesyoon1@users.noreply.github.com>
2022-04-20 14:05:40 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        cyan: colors.cyan,
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
